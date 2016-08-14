d312a6b540694a97fea7b108dea92cb613b11a7a
Pan Wenbin <panwenbin@gmail.com>
2016-08-15 01:03:12 +0800

    添加formatter组件，设置格式化时间样式

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'formatter' => [
+            'class' => 'yii\i18n\Formatter',
+            'dateFormat' => 'php:Y-m-d',
+            'datetimeFormat' => 'php:Y-m-d H:i:s',
+            'timeFormat' => 'php:H:i:s',
+        ]
     ],
     'language' => 'zh-CN',
 ];
