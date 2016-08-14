90d510c7906a45ad189223190add220c5536604a
Pan Wenbin <panwenbin@gmail.com>
2016-08-15 00:40:23 +0800

    gii生成Post模型

---
+++ common/models/Post.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: Pan Wenbin <panwenbin@gmail.com>
+ */
+
+namespace common\models;
+
+
+use common\models\gii\PostGii;
+
+class Post extends PostGii
+{
+
+}
---
+++ common/models/gii/PostGii.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace common\models\gii;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%post}}".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $title
+ * @property string $content
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class PostGii extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%post}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'title', 'content'], 'required'],
+            [['user_id', 'created_at', 'updated_at'], 'integer'],
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => '用户ID',
+            'title' => '标题',
+            'content' => '内容',
+            'created_at' => '发布时间',
+            'updated_at' => '修改时间',
+        ];
+    }
+}
