6e4d4b3df96d478d20ffb2d8ec8edaaba61ecf0d
Pan Wenbin <panwenbin@gmail.com>
2017-01-19 23:16:07 +0800

    about页面使用body-content包裹，与日志样式统一

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -9,5 +9,7 @@ $this->title = '关于我';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <?= $aboutMd ? Markdown::process($aboutMd->content, 'gfm') : '<code>请在后台发表一个标题为about的日志，内容就会显示在这一页。</code>' ?>
+    <div class="body-content">
+        <?= $aboutMd ? Markdown::process($aboutMd->content, 'gfm') : '<code>请在后台发表一个标题为about的日志，内容就会显示在这一页。</code>' ?>
+    </div>
 </div>
