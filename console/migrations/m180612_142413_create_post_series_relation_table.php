dfe275d2d050370dc93c49af03f487e37bebdfec
Pan Wenbin <panwenbin@gmail.com>
2018-06-14 23:15:24 +0800

    修改系列post_series_relation表的编码

--- console/migrations/m180612_142413_create_post_series_relation_table.php
+++ console/migrations/m180612_142413_create_post_series_relation_table.php
@@ -10,21 +10,27 @@ class m180612_142413_create_post_series_relation_table extends Migration
     /**
      * {@inheritdoc}
      */
-    public function safeUp()
+    public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%post_series_relation}}', [
             'id' => $this->primaryKey(),
             'post_title' => $this->string()->notNull()->unique()->comment('日志标题'),
             'series_id' => $this->integer()->comment('系列ID'),
             'order' => $this->smallInteger()->comment('顺序'),
-        ]);
+        ], $tableOptions);
         $this->createIndex('series_order', '{{%post_series_relation}}', ['series_id', 'order']);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function safeDown()
+    public function down()
     {
         $this->dropTable('{{%post_series_relation}}');
     }
