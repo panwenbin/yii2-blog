35f9f44992394d255db9c531b029313711f9cdf1
Pan Wenbin <panwenbin@gmail.com>
2018-06-20 19:47:53 +0800

    补上后台控制器登录限制

--- backend/controllers/SeriesController.php
+++ backend/controllers/SeriesController.php
@@ -5,6 +5,7 @@ namespace backend\controllers;
 use Yii;
 use common\models\Series;
 use common\models\SeriesSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,15 @@ class SeriesController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
