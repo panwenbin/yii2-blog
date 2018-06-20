a4919798200e7e9734418d1f78226cc4f8745056
Pan Wenbin <panwenbin@gmail.com>
2018-06-20 22:39:34 +0800

    文字图标修正

--- backend/adminlte-views/layouts/left.php
+++ backend/adminlte-views/layouts/left.php
@@ -12,7 +12,7 @@ $user = Yii::$app->user->identity;
                 'items' => [
                     ['label' => '后台菜单', 'options' => ['class' => 'header']],
                     ['label' => '日志管理', 'icon' => 'file-code-o', 'url' => ['/post']],
-                    ['label' => '系列管理', 'icon' => 'file-code-o', 'url' => ['/series']],
+                    ['label' => '系列管理', 'icon' => 'book', 'url' => ['/series']],
                     ['label' => '友情链接', 'icon' => 'link', 'url' => ['/link']],
                     ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'], 'visible' => YII_DEBUG],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
